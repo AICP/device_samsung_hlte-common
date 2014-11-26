bb9976917ce07b6e12a281bbdacfd802c0c54daa
Christer <christer.bjor@gmail.com>
2014-11-26 23:51:13 +0100

    hlte: Import QCOM SELinux policies

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,6 +77,9 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_USES_MMCUTILS := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
+# SELinux
+-include device/qcom/sepolicy/sepolicy.mk
+
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true
 BOARD_WLAN_DEVICE := bcmdhd
