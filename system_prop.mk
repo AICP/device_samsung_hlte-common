ee87db23231554e2587c4b400de0b723d6a650c9
Janson Kang <temasek71@gmail.com>
2014-11-30 01:26:12 +0800

    Enable lazy dexopt

--- system_prop.mk
+++ system_prop.mk
@@ -38,3 +38,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
     wifi.supplicant_scan_interval=15
+
+# LAZY DEXOPT
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.lazy.dexopt=true
