346fee4766041b3614a6069db0e96097ff602cc7
Janson Kang <temasek71@gmail.com>
2014-12-11 21:08:57 +0800

    Update BoardConfigCommon.mk for f2fs

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,6 +76,7 @@ BOARD_RECOVERY_SWIPE := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_USES_MMCUTILS := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
