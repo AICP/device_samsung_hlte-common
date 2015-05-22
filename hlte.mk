e0065cbf4ee9dffed1949f740e1e574e5956b19a
Arne Coucheron <arco68@gmail.com>
2015-05-22 11:57:07 +0300

    hlte-common: build custom doze service
    Change-Id: Ie66214f5bc310131604aa4678c69813a28da8f37

--- hlte.mk
+++ hlte.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Doze
+PRODUCT_PACKAGES += \
+    SamsungDoze
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8974
